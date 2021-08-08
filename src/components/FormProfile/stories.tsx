b29e0956da9f424e1a193f8de6228dcd78bfbc15
Arthur Maskalenkas <maskalenkasdoc@gmail.com>
2021-08-08 16:26:38 -0300

    isso deve estruturar o formulario de profile

---
+++ src/components/FormProfile/stories.tsx
@@ -0,0 +1,17 @@
+import { Story, Meta } from '@storybook/react/types-6-0';
+import FormProfile from '.';
+
+export default {
+  title: 'Form/FormProfile',
+  component: FormProfile,
+  args: {},
+  parameters: {
+    layout: 'fullscreen',
+  },
+} as Meta;
+
+export const Default: Story = () => (
+  <div style={{ maxWidth: 860, margin: 'auto' }}>
+    <FormProfile />
+  </div>
+);
