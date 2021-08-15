e6a2ba3aa3f14f1d6c1ba7380e55ad03597b7793
Arthur Maskalenkas <maskalenkasdoc@gmail.com>
2021-08-15 11:55:18 -0300

    isso deve refatorar as margens

--- src/templates/Home/styles.ts
+++ src/templates/Home/styles.ts
@@ -6,7 +6,7 @@ import * as HighlightStyles from 'components/Highlight/styles';
 
 export const SectionBanner = styled.section`
   ${({ theme }) => css`
-    margin: 0 calc(-${theme.grid.gutter} / 2) ${theme.spacings.large};
+    margin: 0 calc(-${theme.grid.gutter}) ${theme.spacings.large};
     ${media.greaterThan('medium')`
       margin-bottom: ${theme.spacings.large};
       position: relative;
