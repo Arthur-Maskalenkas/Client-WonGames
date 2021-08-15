081345e8f1e8702891ac6cb69774486c5c05b856
Arthur Maskalenkas <maskalenkasdoc@gmail.com>
2021-08-15 12:31:04 -0300

    refatoramento

--- src/components/ShowCase/styles.ts
+++ src/components/ShowCase/styles.ts
@@ -6,7 +6,6 @@ import * as HeadingStyles from 'components/Heading/styles';
 import * as GameCardSliderStyles from 'components/GameCardSlider/styles';
 import * as HighlightStyles from 'components/Highlight/styles';
 
-// Modificando o atributo de div para section
 export const Wrapper = styled(Container).attrs({ as: 'section' })`
   ${({ theme }) => css`
     ${HeadingStyles.Wrapper},
@@ -14,17 +13,20 @@ export const Wrapper = styled(Container).attrs({ as: 'section' })`
     ${GameCardSliderStyles.Wrapper} {
       margin-bottom: ${theme.spacings.medium};
     }
+
     ${HighlightStyles.Wrapper} {
       ${media.lessThan('medium')`
-        margin-right: calc(-${theme.grid.gutter});
-        margin-left: calc(-${theme.grid.gutter});
+        margin-right: calc(-${theme.grid.gutter} / 2);
+        margin-left: calc(-${theme.grid.gutter} / 2);
       `}
     }
+
     ${GameCardSliderStyles.Wrapper} {
       ${media.lessThan('huge')`
-        margin-right: calc(-${theme.grid.gutter});
+        margin-right: calc(-${theme.grid.gutter} / 2);
       `}
     }
+
     margin-bottom: calc(${theme.spacings.large} * 2);
   `}
 `;
