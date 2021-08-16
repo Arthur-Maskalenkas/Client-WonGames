1bc1736a6eda38fdfe8c0b0dc6580d82436b9a2e
Arthur Maskalenkas <maskalenkasdoc@gmail.com>
2021-08-16 10:59:18 -0300

    refac

--- src/templates/Base/index.tsx
+++ src/templates/Base/index.tsx
@@ -11,7 +11,7 @@ export type BaseTemplateProps = {
 const Base = ({ children }: BaseTemplateProps) => (
   <S.Wrapper>
     <Container>
-      <Menu username="Arthur" />
+      <Menu />
     </Container>
 
     <S.Content>{children}</S.Content>
