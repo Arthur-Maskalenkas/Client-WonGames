456257d84b692224904b90c7e7011fd1c7458d5a
Arthur Maskalenkas <maskalenkasdoc@gmail.com>
2021-08-17 06:36:15 -0300

    isso deve adicionar o apollo

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,7 @@
 import { AppProps } from 'next/app';
 import Head from 'next/head';
+
+import { ApolloProvider } from '@apollo/client';
 import { ThemeProvider } from 'styled-components';
 
 import GlobalStyles from 'styles/global';
@@ -8,18 +10,20 @@ import theme from 'styles/theme';
 function App({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider theme={theme}>
-      <Head>
-        <title>Won Games</title>
-        <link rel="shortcut icon" href="/img/icon-512.png" />
-        <link rel="apple-touch-icon" href="/img/icon-512.png" />
-        <link rel="manifest" href="/manifest.json" />
-        <meta
-          name="description"
-          content="A simple project starter to work with TypeScript, React, NextJS and Styled Components"
-        />
-      </Head>
-      <GlobalStyles />
-      <Component {...pageProps} />
+      <ApolloProvider>
+        <Head>
+          <title>Won Games</title>
+          <link rel="shortcut icon" href="/img/icon-512.png" />
+          <link rel="apple-touch-icon" href="/img/icon-512.png" />
+          <link rel="manifest" href="/manifest.json" />
+          <meta
+            name="description"
+            content="A simple project starter to work with TypeScript, React, NextJS and Styled Components"
+          />
+        </Head>
+        <GlobalStyles />
+        <Component {...pageProps} />
+      </ApolloProvider>
     </ThemeProvider>
   );
 }
