1cc67b0b8f0e644547c1dfc2ea0bde96ed2e72f2
Arthur Maskalenkas <maskalenkasdoc@gmail.com>
2021-08-26 13:35:00 -0300

    isso deve renderizar os games na parte do client

--- src/templates/Games/index.tsx
+++ src/templates/Games/index.tsx
@@ -1,18 +1,29 @@
+import { useQuery } from '@apollo/client';
+
 import ExploreSidebar, { ItemProps } from 'components/ExploreSidebar';
 import GameCard, { GameCardProps } from 'components/GameCard';
 import { Grid } from 'components/Grid';
 import Base from 'templates/Base';
 
 import * as S from './styles';
-
 import { KeyboardArrowDown as ArrowDown } from '@styled-icons/material-outlined/KeyboardArrowDown';
+
+import { QueryGames, QueryGamesVariables } from 'graphql/generated/QueryGames';
+import { QUERY_GAMES } from 'graphql/queries/games';
 
 export type GamesTemplateProps = {
   games?: GameCardProps[];
   filterItems: ItemProps[];
 };
 
-const GamesTemplate = ({ filterItems, games = [] }: GamesTemplateProps) => {
+const GamesTemplate = ({ filterItems }: GamesTemplateProps) => {
+  const { data, loading, error } = useQuery<QueryGames, QueryGamesVariables>(
+    QUERY_GAMES,
+    {
+      variables: { limit: 15 },
+    },
+  );
+
   const handleFilter = () => {
     return;
   };
@@ -25,19 +36,29 @@ const GamesTemplate = ({ filterItems, games = [] }: GamesTemplateProps) => {
     <Base>
       <S.Main>
         <ExploreSidebar items={filterItems} onFilter={handleFilter} />
+        {loading ? (
+          <p>loading...</p>
+        ) : (
+          <section>
+            <Grid>
+              {data?.games.map((game) => (
+                <GameCard
+                  key={game.slug}
+                  title={game.name}
+                  developer={game.developers[0].name}
+                  slug={game.slug}
+                  img={`${game.cover?.url}`}
+                  price={game.price}
+                />
+              ))}
+            </Grid>
 
-        <section>
-          <Grid>
-            {games.map((item) => (
-              <GameCard key={item.title} {...item} />
-            ))}
-          </Grid>
-
-          <S.ShowMore role="button" onClick={handleShowMore}>
-            <p>Show More</p>
-            <ArrowDown size={35} />
-          </S.ShowMore>
-        </section>
+            <S.ShowMore role="button" onClick={handleShowMore}>
+              <p>Show More</p>
+              <ArrowDown size={35} />
+            </S.ShowMore>
+          </section>
+        )}
       </S.Main>
     </Base>
   );
