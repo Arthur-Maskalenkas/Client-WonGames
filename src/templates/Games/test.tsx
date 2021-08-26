c6960129a094d42b15bcae84c8be9be2ad30b6cd
Arthur Maskalenkas <maskalenkasdoc@gmail.com>
2021-08-26 17:57:47 -0300

    isso deve adicionar e refatorar os testes do template de games

--- src/templates/Games/test.tsx
+++ src/templates/Games/test.tsx
@@ -1,9 +1,10 @@
-import { screen } from '@testing-library/react';
 import { renderWithTheme } from 'utils/tests/helpers';
-import gamesMock from 'components/GameCardSlider/mock';
+import { MockedProvider } from '@apollo/client/testing';
+import { screen } from '@testing-library/react';
 import filterItemsMock from 'components/ExploreSidebar/mock';
 
 import Games from '.';
+import { QUERY_GAMES } from 'graphql/queries/games';
 
 jest.mock('templates/Base', () => ({
   __esModule: true,
@@ -19,21 +20,68 @@ jest.mock('components/ExploreSidebar', () => ({
   },
 }));
 
-jest.mock('components/GameCard', () => ({
-  __esModule: true,
-  default: function Mock() {
-    return <div data-testid="Mock GameCard" />;
-  },
-}));
-
 describe('<Games />', () => {
-  it('should render sections', () => {
+  it('vai renderizar o loading quando o template for carregado', () => {
     renderWithTheme(
-      <Games filterItems={filterItemsMock} games={[gamesMock[0]]} />,
+      <MockedProvider mocks={[]} addTypename={false}>
+        <Games filterItems={filterItemsMock} />
+      </MockedProvider>,
     );
 
-    expect(screen.getByTestId('Mock ExploreSidebar')).toBeInTheDocument();
-    expect(screen.getByTestId('Mock GameCard')).toBeInTheDocument();
+    expect(screen.getByText(/loading.../i)).toBeInTheDocument();
+  });
+
+  // Tem que ser igualzinho ao template
+  // const { data, loading, fetchMore } = useQueryGames({
+  //   variables: { limit: 15 },
+  // });
+
+  it('vai renderizar as sessões', async () => {
+    renderWithTheme(
+      <MockedProvider
+        mocks={[
+          {
+            request: {
+              query: QUERY_GAMES,
+              variables: { limit: 15 },
+            },
+            result: {
+              data: {
+                games: [
+                  {
+                    name: 'Mundaun',
+                    slug: 'mundaun',
+                    cover: {
+                      url: 'https://res.cloudinary.com/won-games/image/upload/v1621522980/mundaun_9164cd782e.jpg',
+                    },
+                    developers: [{ name: 'Hidden Fields' }],
+                    price: 28.49,
+                    __typename: 'Game',
+                  },
+                ],
+              },
+            },
+          },
+        ]}
+        addTypename={false}
+      >
+        <Games filterItems={filterItemsMock} />
+      </MockedProvider>,
+    );
+
+    // Vai iniciar sem dado
+    expect(screen.getByText(/loading.../i)).toBeInTheDocument();
+
+    // get -> tem certeza do elemento
+    // query -> Não tem o elemento
+    // find -> processos assincronos
+
+    // Quando ter dado vai testar os elementos
+    expect(
+      await screen.findByTestId('Mock ExploreSidebar'),
+    ).toBeInTheDocument();
+
+    expect(await screen.findByText(/mundaun/i)).toBeInTheDocument();
 
     expect(
       screen.getByRole('button', { name: /show more/i }),
