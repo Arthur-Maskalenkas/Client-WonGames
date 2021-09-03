6af5f56f602d63a0f00d9b6686f084528f9d04d3
Arthur Maskalenkas <maskalenkasdoc@gmail.com>
2021-09-03 09:09:51 -0300

    refatoramento

--- src/hooks/use-cart/index.tsx
+++ src/hooks/use-cart/index.tsx
@@ -1,12 +1,21 @@
+import { useQueryGames } from 'graphql/queries/games';
 import { useEffect } from 'react';
 import { useState, useContext, createContext } from 'react';
 import { getStorageItem } from 'utils/localStorage';
 
 const CART_KEY = 'cartItems';
 
-export type CartContextData = { items: string[] };
+type CartItem = {
+  id: string;
+  img: string;
+  title: string;
+  price: string;
+};
 
-// Toda a lógica do carrinho estará aqui
+// Props do cartContext
+export type CartContextData = { items: CartItem[] };
+
+// Toda a lógica do carrinho estará aqui.
 export const CartContextDefaultValues = { items: [] };
 
 export const CartContext = createContext<CartContextData>(CartContextDefaultValues);
@@ -15,11 +24,13 @@ export type CartproviderProps = {
   children: React.ReactNode;
 };
 
-// Vai passar todos os valores
+// <CartProvider>{children}</CartProvider>
 const CartProvider = ({ children }: CartproviderProps) => {
+  // Vai possuir apenas ids
   const [cartItems, setCartItems] = useState<string[]>([]);
 
   // No next não tem window na geração do static/ssr. Por isso eu vou driblar isso a partir do useEffect
+  // Pegando os valores definidios através do setStorageItem
   useEffect(() => {
     const data = getStorageItem(CART_KEY);
 
@@ -28,8 +39,19 @@ const CartProvider = ({ children }: CartproviderProps) => {
     }
   }, []);
 
+  const { data } = useQueryGames({
+    // Se não tiver id, não roda
+    skip: !cartItems?.length,
+    variables: {
+      where: {
+        id: cartItems,
+      },
+    },
+  });
+
+  // Disponibilizando os items pego pelo storage
   return (
-    <CartContext.Provider value={{ items: cartItems }}>{children}</CartContext.Provider>
+    <CartContext.Provider value={{ items: data?.games }}>{children}</CartContext.Provider>
   );
 };
 
@@ -37,3 +59,8 @@ const useCart = () => useContext(CartContext);
 // const { items, quantity } = useCart()
 
 export { CartProvider, useCart };
+
+// O carrinho (estado de cartItem) vai funcionar a base de ids, e a query vai buscar essa lista de ids
+// ASsim que carregar o next, o useEffect vai verificar se existem items no storage e passar esses items (ids) para o estado
+// CartContext.provider vai disponibilizar o value
+// CartContextData são as props do CartContext
--- src/hooks/use-cart/test.tsx
+++ src/hooks/use-cart/test.tsx
@@ -1,19 +1,27 @@
+import { MockedProvider } from '@apollo/client/testing';
 import { renderHook } from '@testing-library/react-hooks';
+
 import { setStorageItem } from 'utils/localStorage';
 
 import { useCart, CartProvider, CartproviderProps } from '.';
+import { cartItems, gamesMock } from './mock';
 
 describe('useCart', () => {
-  it('vai retornar items e info de qualquer coisa no carro ', () => {
+  it('vai retornar items e info de qualquer coisa no carrinho ', async () => {
     const wrapper = ({ children }: CartproviderProps) => (
-      <CartProvider>{children}</CartProvider>
+      <MockedProvider mocks={[gamesMock]}>
+        <CartProvider>{children}</CartProvider>
+      </MockedProvider>
     );
 
     setStorageItem('cartItems', ['1', '2']);
 
     // Renderizando dentro do wrapper o useCart
-    const { result } = renderHook(() => useCart(), { wrapper });
+    const { result, waitForNextUpdate } = renderHook(() => useCart(), { wrapper });
 
-    expect(result.current.items).toStrictEqual(['1', '2']);
+    // Espera até que atualize algo
+    await waitForNextUpdate();
+
+    expect(result.current.items).toStrictEqual(cartItems);
   });
 });
