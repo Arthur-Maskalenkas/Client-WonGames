b3716fb1e2c4dbc26a899bf012917738316c7889
Arthur Maskalenkas <maskalenkasdoc@gmail.com>
2021-09-04 23:36:32 -0300

    renderWithTheme refatorado em um novo arquivo

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,13 @@
-import { AppProps } from 'next/app';
-import Head from 'next/head';
-
+import { CartProvider } from 'hooks/use-cart';
 import { ApolloProvider } from '@apollo/client';
 import { ThemeProvider } from 'styled-components';
 
 import GlobalStyles from 'styles/global';
+
+import { AppProps } from 'next/app';
 import theme from 'styles/theme';
+
+import Head from 'next/head';
 import { useApollo } from 'utils/apollo';
 
 function App({ Component, pageProps }: AppProps) {
@@ -14,18 +16,17 @@ function App({ Component, pageProps }: AppProps) {
   return (
     <ApolloProvider client={client}>
       <ThemeProvider theme={theme}>
-        <Head>
-          <title>Won Games</title>
-          <link rel="shortcut icon" href="/img/icon-512.png" />
-          <link rel="apple-touch-icon" href="/img/icon-512.png" />
-          <link rel="manifest" href="/manifest.json" />
-          <meta
-            name="description"
-            content="A simple project starter to work with TypeScript, React, NextJS and Styled Components"
-          />
-        </Head>
-        <GlobalStyles />
-        <Component {...pageProps} />
+        <CartProvider>
+          <Head>
+            <title>Won Games</title>
+            <link rel="shortcut icon" href="/img/icon-512.png" />
+            <link rel="apple-touch-icon" href="/img/icon-512.png" />
+            <link rel="manifest" href="/manifest.json" />
+            <meta name="description" content="The best Game Stores in the world!" />
+          </Head>
+          <GlobalStyles />
+          <Component {...pageProps} />
+        </CartProvider>
       </ThemeProvider>
     </ApolloProvider>
   );
---
+++ src/utils/test-utils.tsx
@@ -0,0 +1,28 @@
+import { ReactElement } from 'react';
+import { render, RenderOptions } from '@testing-library/react';
+import { CartContext, CartContextData, CartContextDefaultValues } from 'hooks/use-cart';
+
+import { ThemeProvider } from 'styled-components';
+import theme from 'styles/theme';
+
+type CustomRenderProps = {
+  cartProviderProps?: CartContextData;
+} & Omit<RenderOptions, 'queries'>;
+
+const customRender = (
+  ui: ReactElement,
+  {
+    cartProviderProps = CartContextDefaultValues,
+    ...renderOptions
+  }: CustomRenderProps = {},
+) =>
+  render(
+    <ThemeProvider theme={theme}>
+      <CartContext.Provider value={cartProviderProps}>{ui}</CartContext.Provider>
+    </ThemeProvider>,
+    renderOptions,
+  );
+
+// Tudo que eu chamar, vai vim dessa pasta aqui
+export * from '@testing-library/react';
+export { customRender as render };
